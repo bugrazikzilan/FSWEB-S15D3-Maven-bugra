2edaf41864dc9274bb649b58dd89276f44c0e0f5
bugrazikzilan <zikzilan.bugra@gmail.com>
2025-05-23 17:31:40 +0300

    completed 2/2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,80 @@
 package org.example;
 
 
+import org.example.entity.Employee;
+
+import java.util.*;
+
 public class Main {
+    private static Map<Integer, Employee> employeeMap;
+    private static List<Employee> duplicatedEmployees;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        LinkedList<Employee> employees = new LinkedList<>();
+
+        employees.add(new Employee(1,"Bugra", "Han"));
+        employees.add(new Employee(1,"Bugra", "Han"));
+        employees.add(new Employee(1,"Bugra", "Han"));
+        employees.add(new Employee(2,"mehir","han"));
+        employees.add(new Employee(3,"sinek", "han"));
+        employees.add(new Employee(4, "mehmet", "can"));
+
+        System.out.println(findDuplicates(employees).size());
+        System.out.println(findUniques(employees).size());
+        System.out.println(removeDuplicates(employees).size());
+
+
     }
+
+    public static List<Employee> findDuplicates(List<Employee> employees){
+        employeeMap = new HashMap<>();
+        duplicatedEmployees = new LinkedList<>();
+
+        Iterator<Employee> iterator = employees.iterator();
+        while (iterator.hasNext()){
+            Employee employee = iterator.next();
+            if(employee == null){
+                System.out.println("null");
+                continue;
+            }
+            if(employeeMap.containsKey(employee.getId())){
+                duplicatedEmployees.add(employee);
+            }
+
+            else {
+                employeeMap.put(employee.getId(), employee);
+            }
+        }
+        return duplicatedEmployees;
+    }
+
+    public static Map<Integer, Employee> findUniques(List<Employee> employees){
+        employeeMap = new HashMap<>();
+        Iterator<Employee> iterator = employees.iterator();
+        while (iterator.hasNext()){
+            Employee employee = iterator.next();
+            if(employee == null){
+                System.out.println("null");
+                continue;
+            }
+            if(!employeeMap.containsKey(employee.getId())){
+                employeeMap.put(employee.getId(), employee);
+            }
+        }
+        return employeeMap;
+
+    }
+
+
+    public static List<Employee> removeDuplicates(List<Employee> employees){
+        List<Employee> duplicates = findDuplicates(employees);
+        Map<Integer, Employee> uniques = findUniques(employees);
+
+        List<Employee> onlyUnique = new LinkedList<>(uniques.values());
+        onlyUnique.removeAll(duplicates);
+        return onlyUnique;
+
+    }
+
 }
